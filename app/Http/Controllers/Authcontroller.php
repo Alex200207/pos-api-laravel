58f3a152e487e62a2cf3735c52b46ed69402cfce
soy eddy <soyeddy.email.com>
2025-05-18 00:13:48 -0600

    feat: Add register method to AuthController for user registration

--- app/Http/Controllers/Authcontroller.php
+++ app/Http/Controllers/Authcontroller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -9,7 +10,7 @@ class AuthController extends Controller
     // Método para iniciar sesión
     public function login(Request $request)
     {
-        // Validar los datos de entrada 
+        // Validar los datos de entrada
         $request->validate([
             'email' => 'required|email',
             'password' => 'required',
@@ -25,6 +26,28 @@ class AuthController extends Controller
 
         // Autenticación fallida
         return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    public function register(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+       
+        $token = $user->createToken('access_token')->plainTextToken;
+
+        return response()->json(['token' => $token], 201);
     }
 
     // Método para cerrar sesión
